6f39534f6c33e4466248da86dae999b64958cdc5
Adriana Silivestru <adriana.silivestru20@gmail.com>
2026-03-07 23:55:58 +0200

    feat: add verify email functionality

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -1,55 +1,84 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require('../utils');
+const crypto = require("crypto");
 
 const register = async (req, res) => {
-  const { email, name, password } = req.body;
+	const { email, name, password } = req.body;
 
-  const emailAlreadyExists = await User.findOne({ email });
-  if (emailAlreadyExists) {
-    throw new CustomError.BadRequestError('Email already exists');
-  }
+	const emailAlreadyExists = await User.findOne({ email });
+	if (emailAlreadyExists) {
+		throw new CustomError.BadRequestError('Email already exists');
+	}
 
-  // first registered user is an admin
-  const isFirstAccount = (await User.countDocuments({})) === 0;
-  const role = isFirstAccount ? 'admin' : 'user';
+	// first registered user is an admin
+	const isFirstAccount = (await User.countDocuments({})) === 0;
+	const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+	const verificationToken = crypto.randomBytes(40).toString("hex");
+	const user = await User.create({ name, email, password, role, verificationToken });
+	const origin = "http://localhost:3000";
+	await sendVerificationEmail({ name: user.name, email: user.email, verificationToken: user.verificationToken, origin });
+	res.status(StatusCodes.OK).json({ msg: "Please check your email to verify the account" });
 };
 const login = async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  if (!email || !password) {
-    throw new CustomError.BadRequestError('Please provide email and password');
-  }
-  const user = await User.findOne({ email });
+	if (!email || !password) {
+		throw new CustomError.BadRequestError('Please provide email and password');
+	}
+	const user = await User.findOne({ email });
 
-  if (!user) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials');
-  }
-  const isPasswordCorrect = await user.comparePassword(password);
-  if (!isPasswordCorrect) {
-    throw new CustomError.UnauthenticatedError('Invalid Credentials');
-  }
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
+	if (!user) {
+		throw new CustomError.UnauthenticatedError('Invalid Credentials');
+	}
+	const isPasswordCorrect = await user.comparePassword(password);
+	if (!isPasswordCorrect) {
+		throw new CustomError.UnauthenticatedError('Invalid Credentials');
+	}
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+	if (!user.isVerified) {
+		throw new CustomError.UnauthenticatedError("Please verify your account in the email");
+	}
+	const tokenUser = createTokenUser(user);
+	attachCookiesToResponse({ res, user: tokenUser });
+
+	res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000),
-  });
-  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+	res.cookie('token', 'logout', {
+		httpOnly: true,
+		expires: new Date(Date.now() + 1000),
+	});
+	res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+};
+
+const verifyEmail = async (req, res) => {
+	const { verificationToken, email } = req.body;
+	if (!email || !verificationToken) {
+		throw new CustomError.BadRequestError("Please provide email and verification token");
+	}
+
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw new CustomError.UnauthenticatedError(`User not found`);
+	}
+
+	if (verificationToken !== user.verificationToken) {
+		throw new CustomError.UnauthenticatedError("Verification token not correct");
+	}
+
+	user.isVerified = true;
+	user.verified = Date.now();
+	user.verificationToken = "";
+	await user.save();
+	res.status(StatusCodes.OK).json({ msg: "Email verified" });
 };
 
 module.exports = {
-  register,
-  login,
-  logout,
+	register,
+	login,
+	logout,
+	verifyEmail
 };
